5deaf37bf34b2ba01367c20aad219d2fc4f1428a
Sam Richards <richards.m.samuel@gmail.com>
2023-06-10 18:09:19 +1200

    Changed twitter url to use vx twitter for embeds

--- src/bot.ts
+++ src/bot.ts
@@ -11,7 +11,7 @@ const twitterUsername = process.env.TWITTER_USERNAME;
 const twitterPassword = process.env.TWITTER_PASSWORD;
 
 const meowicTwitterId = process.env.MEOWIC_TWITTER_ID;
-const discordChannelId = process.env.DISCORD_CHANNEL_ID;
+let discordChannelId = process.env.DISCORD_CHANNEL_ID;
 
 if (!twitterEmail || !twitterUsername || !twitterPassword) {
     console.error('Please define TWITTER_EMAIL, TWITTER_USERNAME, and TWITTER_PASSWORD environment variables.');
@@ -28,49 +28,59 @@ const client = new Client({
     ],
 })
 
-client.once('ready', () => {
-    console.log('Ready!');
-});
-
-client.login(process.env.DISCORD_TOKEN);
-
-client.on("messageCreate", async (message) => {
-    console.log(message.content)
-});
 
 // Create an instance of the TwitterLikesService which is a promise
 const userServicePromise = getUserService();
 
-async function init() {
-    const userService = await userServicePromise;
-    const twitterLikesService = new TwitterLikesService(userService);
-
-    // Function to check for changes in likes and send a Discord notification
-    async function checkAndNotifyChanges() {
-        const newLikesCursoredData = await twitterLikesService.getNewLikes(meowicTwitterId);
-        let newLikes = newLikesCursoredData.list;
-
-        if (newLikes.length > 5) {
-            newLikes = newLikes.slice(0, 5);
-        }
-        // reverse the array for chronological order
-        newLikes = newLikes.reverse();
-        for (const like of newLikes) {
-            // Send Discord notification
+async function checkAndNotifyChanges(userService, twitterLikesService) {
+    const newLikesCursoredData = await twitterLikesService.getNewLikes(meowicTwitterId);
+    let newLikes = newLikesCursoredData.list;
+    
+    if (newLikes.length > 10) {
+        newLikes = newLikes.slice(0, 10);
+    }
+    // reverse the array for chronological order
+    newLikes = newLikes.reverse();
+    for (const like of newLikes) {
+        // Send Discord notification
+        try {
             const channel = await client.channels.fetch(discordChannelId) as TextChannel; 
+            if (!channel) {
+                console.error(`Channel with ID ${discordChannelId} not found`);
+                continue;
+            }
             const user = await userService.getUserDetails(like.tweetBy);
-            const likeUrl = `https://twitter.com/${user.userName}/status/${like.id}`
-            const messageContent = `New Meowic like!\n\n${likeUrl}`;
+            //const likeUrl = `https://twitter.com/${user.userName}/status/${like.id}`
+            const likeUrl = `https://vxtwitter.com/${user.userName}/status/${like.id}`
+            const messageContent = `Meowic liked this\n${likeUrl}`;
             channel.send(messageContent);
+        } catch(err) {
+            console.error(`Failed to send message to channel: ${err}`);
         }
     }
+}
 
-    // Run once immediately on first load
-    checkAndNotifyChanges();
-
-    // Set an interval to periodically check for new likes
-    setInterval(checkAndNotifyChanges, (10 + Math.random() * 15) * 60 * 1000); // Random time between 5 and 10 minutes
+async function init() {
+    const userService = await userServicePromise;
+    userServicePromise.catch((error) => {
+        console.error('Error in userServicePromise:', error);
+    });
+    const twitterLikesService = new TwitterLikesService(userService);
+    client.once('ready', () => {
+        console.log('Ready!');
+        
+        // Run once immediately on first load
+        //checkAndNotifyChanges(userService, twitterLikesService);
+        
+        // Set an interval to periodically check for new likes
+        setInterval(() => checkAndNotifyChanges(userService, twitterLikesService), (15 + Math.random() * 25) * 60 * 1000);
+    });
+    
 }
 
 //start main cycle
-init();
+(async () => {
+    await init();
+})();
+
+client.login(process.env.DISCORD_TOKEN);
